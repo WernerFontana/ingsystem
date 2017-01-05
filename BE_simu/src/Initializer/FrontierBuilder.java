f4fb9fcf0f4371017573965c5f89e32cf23d79b4
HVadaine <hugo.vadaine@ensta-bretagne.org>
2017-01-05 12:43:09 +0100

    correction des cross et frontier builder

--- BE_simu/src/Initializer/FrontierBuilder.java
+++ BE_simu/src/Initializer/FrontierBuilder.java
@@ -3,19 +3,20 @@ package Initializer;
 import org.json.JSONArray;
 
 import config.DataManager;
+import engine.impl.BasicSimEngine;
 import entity.Environment;
 import entity.Frontier;
 import entity.Line;
 
 public class FrontierBuilder {
 		
-		public FrontierBuilder(DataManager bdd,Environment env)
+		public FrontierBuilder(DataManager bdd,Environment env,BasicSimEngine engine)
 		{
 			JSONArray jsonArray = (JSONArray)bdd.getLanes(); 
 			if (jsonArray != null) { 
 			   int len = jsonArray.length();
 			   for (int i=0;i<len;i++){ 
-			  env.addNode(new Frontier((Integer) bdd.getLaneItem("id", i)));
+			  env.addNode(new Frontier(engine,(Integer) bdd.getLaneItem("id", i)));
 			   } 
 			} 
 		}
