89c61d8f642fc96037c5f58ebe1b73bc8256edba
OpesMentis <planchot.antoine@gmail.com>
2017-01-05 13:24:19 +0100

    Mise à jour de la base de données et création des méthodes associées.

--- BE_simu/src/config/DataManager.java
+++ BE_simu/src/config/DataManager.java
@@ -34,4 +34,19 @@ public class DataManager {
 	public JSONArray getFrontier(){
 		return bdd.getJSONArray("frontier");
 	}
+	
+	public int getFreq(int idFront, int idHor) {
+		/* idHor : 0h-7h -> 0
+		 * 		   7h-9h -> 1
+		 * 		   9h-17h -> 2
+		 * 		   17h-19h -> 3
+		 * 		   19h-0h -> 4
+		 */
+		
+		return bdd.getJSONArray("frequence").getJSONArray(idFront-1).getInt(idHor);
+	}
+	
+	public int getAttract(int idSource, int idCible) {
+		return bdd.getJSONArray("attrait").getJSONArray(idSource-1).getInt(idCible-1);
+	}
 }
