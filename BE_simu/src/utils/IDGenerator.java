3094d4be022b37d5de596e884ad60fd43e73351a
HVadaine <hugo.vadaine@ensta-bretagne.org>
2017-01-19 19:22:40 +0100

    add ID generator for car

---
+++ BE_simu/src/utils/IDGenerator.java
@@ -0,0 +1,41 @@
+package utils;
+
+import java.util.LinkedList;
+
+import config.DataManager;
+
+public class IDGenerator {
+	private static IDGenerator self = new IDGenerator();
+	private LinkedList<Integer> IDList;
+	
+	public IDGenerator()
+	{
+		int carNumber=0;
+		DataManager DM= new DataManager();
+		for(int i=1;i<7;i++)
+		{
+			for(int k:DM.getFreq(i))
+			{
+				carNumber+=k;
+			}
+		}
+		carNumber=(int) (carNumber*1.3);
+		IDList = new LinkedList<>();
+		for(int i =0;i<carNumber;i++)
+		{
+			IDList.add(i);
+		}
+	}
+	
+	public static IDGenerator getSelf()
+	{
+		return self;
+	}
+	
+	public int getNext()
+	{
+		IDList.removeFirst();
+		return IDList.getFirst();
+	}
+
+}
